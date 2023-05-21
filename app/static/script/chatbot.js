11c49815ee3e50f11c66456c6a93f61335bf6899
root <root@LAPTOP-TIMRJRMU>
2023-05-22 06:44:40 +0800

    added functionality for login/signup

--- app/static/script/chatbot.js
+++ app/static/script/chatbot.js
@@ -98,76 +98,76 @@ $.ajaxSetup({
 */
 
 $('#form_register').on('submit', function(e) {
-    e.preventDefault();
-    calculateQTc();
+	e.preventDefault();
+	calculateQTc();
 });
 
 
 
 function getContactFormData(form) {
-    // creates a FormData object and adds chips text
-    var formData = new FormData(document.getElementById(form));
-//    for (var [key, value] of formData.entries()) { console.log('formData', key, value);}
-    return formData
+	// creates a FormData object and adds chips text
+	var formData = new FormData(document.getElementById(form));
+	//    for (var [key, value] of formData.entries()) { console.log('formData', key, value);}
+	return formData
 }
 
-function calculateQTc(){
-    alert("hi");
-    var server_side_errors = document.getElementById("server_side_errors");
+function calculateQTc() {
+	alert("hi");
+	var server_side_errors = document.getElementById("server_side_errors");
 
-    var name = document.getElementById("name").value.trim();
-     var username = document.getElementById("username").value.trim();
-     var email = document.getElementById("email").value.trim();
-     var phonenumber = document.getElementById("phonenumber").value.trim();
-     var password = document.getElementById("password").value.trim();
+	var name = document.getElementById("name").value.trim();
+	var username = document.getElementById("username").value.trim();
+	var email = document.getElementById("email").value.trim();
+	var phonenumber = document.getElementById("phonenumber").value.trim();
+	var password = document.getElementById("password").value.trim();
 
-     alert(name);
+	alert(name);
 
-    checkname = validateName(name);
-    checkusername = validateUsername(username);
-    checkemail = validateEmail(email);
-    checknumber = validatePhoneNumber(phonenumber);
-    checkpassword = ValidatePassword(password);
-     if(checkname && checkusername && checkemail && checknumber && checkpassword){
-         // If all fields are valid, you can proceed with form submission
-     if (name !== '' && username !== ''&& email !== ''&& phonenumber !== ''&& password !== ''  ) {
-       
-        var form_id = 'form_register';
-        
-        var formData = getContactFormData(form_id);
-        
-        //var url = form.prop('action');
-        var form = $('#form_register');
-        var type = form.prop('method');
-        var url = form.prop('action');
-        alert('only');
+	checkname = validateName(name);
+	checkusername = validateUsername(username);
+	checkemail = validateEmail(email);
+	checknumber = validatePhoneNumber(phonenumber);
+	checkpassword = ValidatePassword(password);
+	if (checkname && checkusername && checkemail && checknumber && checkpassword) {
+		// If all fields are valid, you can proceed with form submission
+		if (name !== '' && username !== '' && email !== '' && phonenumber !== '' && password !== '') {
 
-    
-           $.ajax({
-            url: url,
-            type: type,
-            data: formData,
-            processData: false,
-            contentType: false,
-             success: function(result) {
-                alert(result.flag);
-                alert(result.error_msg);
-                if(result.flag == 1){
-                    window.location.replace("/");
-                }else{
-                    $("#server_side_errors").css("display", "none");
-                    //alert(result.error_message_email);
-                    server_side_errors.innerHTML = result.error_msg; 
-                    $("#server_side_errors").css("display", "block");
-                }
-             
+			var form_id = 'form_register';
 
-             } 
-           });
-     }
-    }
+			var formData = getContactFormData(form_id);
 
-    
+			//var url = form.prop('action');
+			var form = $('#form_register');
+			var type = form.prop('method');
+			var url = form.prop('action');
+			alert('only');
+
+
+			$.ajax({
+				url: url,
+				type: type,
+				data: formData,
+				processData: false,
+				contentType: false,
+				success: function(result) {
+					alert(result.flag);
+					alert(result.error_msg);
+					if (result.flag == 1) {
+						window.location.replace("/");
+					} else {
+						$("#server_side_errors").css("display", "none");
+						//alert(result.error_message_email);
+						server_side_errors.innerHTML = result.error_msg;
+						$("#server_side_errors").css("display", "block");
+					}
+
+
+				}
+			});
+		}
+	}
+
+
 
 }
 
@@ -197,92 +197,88 @@ function calculateQTc(){
 
      });
 */
- 
- // Function to validate first name
- function validateName(name) {
-    const errorElement = document.getElementById("nameErrorMsg");
-    if (name === '') {
-        errorElement.innerHTML = 'Name is required';
-        $("#nameErrorMsg").css("display", "block");
-        return false;
-    } else {
-        errorElement.innerHTML="";
-        $("#nameErrorMsg").css("display", "none");
-        return true;
-    }
-  }
 
-  function validateUsername(username) {
-    const errorElement = document.getElementById("usernameErrorMsg");
-    if (username === '') {
-        alert("hiiii");
-        errorElement.innerHTML = 'Username is required';
-        $("#usernameErrorMsg").css("display", "block");
-        return false;
-    } else {
-        alert(" vooooo");
-        errorElement.innerHTML="";
-        $("#usernameErrorMsg").css("display", "none")
-        return true;
-    }
-  }
+// Function to validate first name
+function validateName(name) {
+	const errorElement = document.getElementById("nameErrorMsg");
+	if (name === '') {
+		errorElement.innerHTML = 'Name is required';
+		$("#nameErrorMsg").css("display", "block");
+		return false;
+	} else {
+		errorElement.innerHTML = "";
+		$("#nameErrorMsg").css("display", "none");
+		return true;
+	}
+}
 
-  function validateEmail(email) {
-    const errorElement = document.getElementById("emailErrorMsg");
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (email === '') {
-        errorElement.innerHTML = 'Email is required';
-        $("#emailErrorMsg").css("display", "block");
-        return false;
-    }else if(!emailRegex.test(email)){
-         errorElement.innerHTML = 'Invalid Email Format';
-         $("#emailErrorMsg").css("display", "block");
-         return false;
-    } 
-    else {
-        errorElement.innerHTML="";
-        $("#emailErrorMsg").css("display", "none")
-        return true;
-    }
-  }
+function validateUsername(username) {
+	const errorElement = document.getElementById("usernameErrorMsg");
+	if (username === '') {
+		alert("hiiii");
+		errorElement.innerHTML = 'Username is required';
+		$("#usernameErrorMsg").css("display", "block");
+		return false;
+	} else {
+		alert(" vooooo");
+		errorElement.innerHTML = "";
+		$("#usernameErrorMsg").css("display", "none")
+		return true;
+	}
+}
 
-  function validatePhoneNumber(phonenumber) {
-    alert(phonenumber);
-    const errorElement = document.getElementById("phonenumberErrorMsg");
-    if (phonenumber === '') {
-        alert("hiiii phone");
-        errorElement.innerHTML = 'Phone Number is required';
-        $("#phonenumberErrorMsg").css("display", "block");
-        return false;
-    }
-    else if(phonenumber.length < 10){
-        alert("phoe");
-        errorElement.innerHTML = 'Number should be 10 Digit long';
-        $("#phonenumberErrorMsg").css("display", "block");
-        return false;
-    }else {
-        errorElement.innerHTML="";
-        $("#phonenumberErrorMsg").css("display", "none");
-        return true;
-    }
-  }
+function validateEmail(email) {
+	const errorElement = document.getElementById("emailErrorMsg");
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (email === '') {
+		errorElement.innerHTML = 'Email is required';
+		$("#emailErrorMsg").css("display", "block");
+		return false;
+	} else if (!emailRegex.test(email)) {
+		errorElement.innerHTML = 'Invalid Email Format';
+		$("#emailErrorMsg").css("display", "block");
+		return false;
+	} else {
+		errorElement.innerHTML = "";
+		$("#emailErrorMsg").css("display", "none")
+		return true;
+	}
+}
+
+function validatePhoneNumber(phonenumber) {
+	alert(phonenumber);
+	const errorElement = document.getElementById("phonenumberErrorMsg");
+	if (phonenumber === '') {
+		alert("hiiii phone");
+		errorElement.innerHTML = 'Phone Number is required';
+		$("#phonenumberErrorMsg").css("display", "block");
+		return false;
+	} else if (phonenumber.length < 10) {
+		alert("phoe");
+		errorElement.innerHTML = 'Number should be 10 Digit long';
+		$("#phonenumberErrorMsg").css("display", "block");
+		return false;
+	} else {
+		errorElement.innerHTML = "";
+		$("#phonenumberErrorMsg").css("display", "none");
+		return true;
+	}
+}
 
 
-  function ValidatePassword(password) {
-    const errorElement = document.getElementById("passwordErrorMsg");
-    if (password === '') {
-        errorElement.innerHTML = 'Password is required';
-        $("#passwordErrorMsg").css("display", "block");
-        return false;
-    }else if(password.length < 8){
-     errorElement.innerHTML = 'Password should be at least 8 characters long';
-     $("#passwordErrorMsg").css("display", "block");
-     return false;
-    } else {
-        errorElement.innerHTML="";
-        $("#passwordErrorMsg").css("display", "none");
-        return true;
-    }
-  }
-
-  
+function ValidatePassword(password) {
+	const errorElement = document.getElementById("passwordErrorMsg");
+	if (password === '') {
+		errorElement.innerHTML = 'Password is required';
+		$("#passwordErrorMsg").css("display", "block");
+		return false;
+	} else if (password.length < 8) {
+		errorElement.innerHTML = 'Password should be at least 8 characters long';
+		$("#passwordErrorMsg").css("display", "block");
+		return false;
+	} else {
+		errorElement.innerHTML = "";
+		$("#passwordErrorMsg").css("display", "none");
+		return true;
+	}
+}
--- app/views.py
+++ app/views.py
@@ -1,4 +1,3 @@
-
 from werkzeug.utils import redirect
 from app import app, db
 from app.models import User
@@ -11,8 +10,10 @@ from flask_wtf.csrf import CSRFProtect
 
 
 import nltk
-nltk.download('popular')
+
+nltk.download("popular")
 from nltk.stem import WordNetLemmatizer
+
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
@@ -25,123 +26,131 @@ from keras.models import load_model
 import uuid
 from app import app, db
 
-model = load_model('model.h5')
+model = load_model("model.h5")
 import json
 import random
-intents = json.loads(open('app/data.json').read())
-words = pickle.load(open('texts.pkl','rb'))
-classes = pickle.load(open('labels.pkl','rb'))
-#DATABASE = 'chat_history.db'
+
+intents = json.loads(open("app/data.json").read())
+words = pickle.load(open("texts.pkl", "rb"))
+classes = pickle.load(open("labels.pkl", "rb"))
+# DATABASE = 'chat_history.db'
 
 csrf = CSRFProtect(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
-    
-@app.route('/signup')
+    return render_template("index.html")
+
+
+@app.route("/signup")
 def signup():
-    return render_template('signup.html')
-    
-    
-@app.route('/register-user', methods=['POST'])
+    return render_template("signup.html")
+
+
+@app.route("/register-user", methods=["POST"])
 def register_user():
-    
-    
+
     if request.method == "POST":
         print("hiiiiiiiiiiiiii")
         print(request.form)
-     
+
     form = request.form
-    error_message_name = ''
-    error_message_email = ''
-    error_message_username = ''
-    error_message_phone_number = ''
-    error_message_password = ''
-    
+    error_message_name = ""
+    error_message_email = ""
+    error_message_username = ""
+    error_message_phone_number = ""
+    error_message_password = ""
+
     flag = 0
-    error_msg= []
-    if form['name'] != '' and form['username'] != '' and form['email-address'] != '' and form['phone-number'] != '' and form['password'] != '':
-        print(form['username'])
-        print(form['email-address'])
-    
-        username = User.query.filter_by(username=form['username']).first()
-        userEmail = User.query.filter_by(email=form['email-address']).first()
-        userPhoneNumber = User.query.filter_by(phone_number=form['phone-number']).first()
+    error_msg = []
+    if (
+        form["name"] != ""
+        and form["username"] != ""
+        and form["email-address"] != ""
+        and form["phone-number"] != ""
+        and form["password"] != ""
+    ):
+        print(form["username"])
+        print(form["email-address"])
+
+        username = User.query.filter_by(username=form["username"]).first()
+        userEmail = User.query.filter_by(email=form["email-address"]).first()
+        userPhoneNumber = User.query.filter_by(
+            phone_number=form["phone-number"]
+        ).first()
         print(username)
         print(userEmail)
-        if username==None and userEmail ==None and userPhoneNumber == None:
-            print('create')
+        if username == None and userEmail == None and userPhoneNumber == None:
+            print("create")
             user = User(
-                name=form['name'],
-                username=form['username'],
-                email=form['email-address'],
-                phone_number=form['phone-number']
+                name=form["name"],
+                username=form["username"],
+                email=form["email-address"],
+                phone_number=form["phone-number"],
             )
-        
-            user.set_password(form['password'])
+
+            user.set_password(form["password"])
             db.session.add(user)
             db.session.commit()
             flag = 1
             error_msg.append("created")
-            resp = {"flag" : flag, "error_msg" : error_msg}
+            resp = {"flag": flag, "error_msg": error_msg}
         else:
             if username != None:
-                print('ggggg')
+                print("ggggg")
                 error_msg.append("User Already exits")
-                #return render_template('signup.html', form=form,error_message  =  error_message)
-            
+                # return render_template('signup.html', form=form,error_message  =  error_message)
+
             if userEmail != None:
 
-                print('nnnnn')
+                print("nnnnn")
                 error_msg.append("Email Already exits")
-                #return render_template('signup.html', form=form,error_message  =  error_message)
-                
+                # return render_template('signup.html', form=form,error_message  =  error_message)
+
             if userPhoneNumber != None:
 
-                print('cc')
+                print("cc")
                 error_msg.append("Phone Already exits")
-            resp = {"flag" : flag, "error_msg" : error_msg} 
-                
-    else:
-        if form['name'] == '':
-            error_msg.append("Name cannot be empty")
-        if form['email-address'] == '':
-            error_msg.append("Email cannot be empty")
-        if form['phone-number'] == '':
-            error_msg.append("Phone Number cannot be empty")
-        if form['password'] == '':
-            error_msg.append("Password cannot be empty")
-        if form['username'] == '':
-            error_msg.append("Username cannot be empty")                 
-            
-        resp = {"flag" : flag, "error_msg" : error_msg}       
-            
-    
-    return jsonify(resp)
-    
+            resp = {"flag": flag, "error_msg": error_msg}
 
-@app.route('/login_user', methods=['POST'])
+    else:
+        if form["name"] == "":
+            error_msg.append("Name cannot be empty")
+        if form["email-address"] == "":
+            error_msg.append("Email cannot be empty")
+        if form["phone-number"] == "":
+            error_msg.append("Phone Number cannot be empty")
+        if form["password"] == "":
+            error_msg.append("Password cannot be empty")
+        if form["username"] == "":
+            error_msg.append("Username cannot be empty")
+
+        resp = {"flag": flag, "error_msg": error_msg}
+
+    return jsonify(resp)
+
+
+@app.route("/login_user", methods=["POST"])
 def login_user():
     form = request.form
-    user = User.query.filter_by(username=form['username']).first()
-    password = form['password']
-    #print(user.password_hash)
-    print("hiiii"+generate_password_hash(password))
-    
+    user = User.query.filter_by(username=form["username"]).first()
+    password = form["password"]
+    # print(user.password_hash)
+    print("hiiii" + generate_password_hash(password))
+
     if not user or not check_password_hash(user.password_hash, password):
         print("wrong")
         flash("Password was incorrect or user doesn't exist.")
-        return render_template('index.html')
+        return render_template("index.html")
     else:
-        
-        session['username'] = form['username']
-        return redirect('/dashboard')
-       # return redirect(url_for('auth.login'))
-       
-       
 
-@app.route('/dashboard')
+        session["username"] = form["username"]
+        return redirect("/dashboard")
+    # return redirect(url_for('auth.login'))
+
+
+@app.route("/dashboard")
 def dashboard():
     print("gggggg")
     if not session.get("username"):
@@ -151,15 +160,12 @@ def dashboard():
         print("hiiii vv")
         return render_template("dashboard.html")
 
-    
-        
-    
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
-   # remove the username from the session if it is there
-   session.pop('username', None)
-   return redirect(url_for('index'))    
-    
+    # remove the username from the session if it is there
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
 
 def clean_up_sentence(sentence):
@@ -169,28 +175,31 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
+
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
 
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
     # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)  
+    bag = [0] * len(words)
     for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
+        for i, w in enumerate(words):
+            if w == s:
                 # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("found in bag: %s" % w)
+    return np.array(bag)
+
 
 def predict_class(sentence, model):
     # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
@@ -198,14 +207,16 @@ def predict_class(sentence, model):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
+
 def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
+    tag = ints[0]["intent"]
+    list_of_intents = intents_json["intents"]
     for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
+        if i["tag"] == tag:
+            result = random.choice(i["responses"])
             break
     return result
+
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
@@ -213,25 +224,7 @@ def chatbot_response(msg):
     return res
 
 
-
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
+    userText = request.args.get("msg")
     return chatbot_response(userText)
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-    
-
-
